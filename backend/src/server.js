99d9e2836081aa9835d6319a1b1e0e1830da7692
Renato Mosca <rmosca2001@hotmail.com>
2019-08-13 20:03:55 -0300

    inclusão de novas libraries

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,16 +2,20 @@ const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
 const bodyparse = require('body-parser');
+const cors = require('cors');
+require('dotenv/config');
 
-mongoose.connect('mongodb+srv://mongoRenato:Mosca-2019@cluster0-tuj14.mongodb.net/dbmongorenato?retryWrites=true&w=majority', {
+
+mongoose.connect(process.env.DB_STRING,{
     useNewUrlParser: true
 });
 
 const app = express();
+app.use(cors()); ///qdo uso cors() vazio, estou liberando para todo mundo
 app.use(bodyparse.json())
 app.use(routes);
 
 
 app.listen(3000, function(){
-    console.log('Servidor ativo porta 3000//');
+    console.log('Servidor ativo porta 3000///');
 })
