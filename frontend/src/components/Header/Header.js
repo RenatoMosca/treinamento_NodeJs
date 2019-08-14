8245b6a2b4d9fb2aab9fca2267d3d98b2e0f172c
Renato Mosca <rmosca2001@hotmail.com>
2019-08-13 22:27:56 -0300

    começando no React

---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="">
+        <AppBar position="static" color="default">
+          <Toolbar>
+            <Typography variant="h6" color="inherit">
+              Treinamento NodeJs
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
+}
+
+export default Header;
